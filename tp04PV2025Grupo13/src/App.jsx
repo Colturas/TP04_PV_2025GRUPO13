e966bf970a278da2bace723abd306a745ba11e80
FranciscoDiaz-01 <d3st31@gmail.com>
2025-05-20 00:13:28 -0300

    Diaz Francisco, realizo minimos retoques en el codigo de Fernandez Jorge

--- tp04PV2025Grupo13/src/App.jsx
+++ tp04PV2025Grupo13/src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
-import './App.css'
-import Product from './assets/components/product';
-function App() {
-  const [] = useState(0)
+//import { useState } from 'react';
+import './App.css';
+import Product from './assets/components/Product';
 
+function App() {
   return (
-    <>
     <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
       <h1>Administración de Productos</h1>
       <Product />
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
